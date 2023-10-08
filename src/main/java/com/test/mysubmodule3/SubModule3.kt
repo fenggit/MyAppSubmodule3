7df163c9bf5d5d06c8a55abb578c870414c09daa
hefeng <hefeng02@agora.io>
2023-10-08 11:38:39 +0800

    first commit

---
+++ src/main/java/com/test/mysubmodule3/SubModule3.kt
@@ -0,0 +1,18 @@
+package com.test.mysubmodule3
+
+/**
+ * author : felix
+ * date : 2023/8/24
+ * description :
+ */
+class SubModule3 {
+    var name = "SubModule3"
+
+    fun setSubModuleName(name: String) {
+        this.name = name
+    }
+
+    fun getSubModuleName(): String {
+        return name
+    }
+}
