da4ada376049cc4685226da43d0103e1b4055c9f
hefeng <hefeng02@agora.io>
2023-10-08 16:04:02 +0800

    dev1 branch

--- src/main/java/com/test/mysubmodule3/SubModule3.kt
+++ src/main/java/com/test/mysubmodule3/SubModule3.kt
@@ -6,7 +6,7 @@ package com.test.mysubmodule3
  * description :
  */
 class SubModule3 {
-    var name = "SubModule3"
+    var name = "SubModule3-dev1"
 
     fun setSubModuleName(name: String) {
         this.name = name
