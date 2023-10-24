746c8dbcf61ee498f2d82d7f140e35e2a9cd58de
hefeng <hefeng02@agora.io>
2023-10-24 11:16:14 +0800

    3333

--- src/main/java/com/test/mysubmodule3/SubModule3.kt
+++ src/main/java/com/test/mysubmodule3/SubModule3.kt
@@ -6,7 +6,7 @@ package com.test.mysubmodule3
  * description :
  */
 class SubModule3 {
-    var name = "SubModule333"
+    var name = "SubModule333x"
 
     fun setSubModuleName(name: String) {
         this.name = name
